fded411585265a96f26134373aa2d288c0759499
seyyar95 <heydrovsyyar@gmail.com>
2023-10-17 09:11:55 -0700

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry poiint
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+		int x = 48;
+
+		while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
