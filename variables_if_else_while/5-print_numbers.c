b3a82b2ae47ef90bd7d3c4e4c4f5665f494a1a65
seyyar95 <heydrovsyyar@gmail.com>
2023-10-17 08:54:44 -0700

    Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-    int x = 0;
+	int x = 0;
 
-    while (x < 10)
-    {
-        printf("%d", x);
-        x++;
-    }
-    printf("\n");
-    return (0);
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
 }
 
