0f32f1c808e2f0341e0c8ccefbba401eaa783b26
seyyar95 <heydrovsyyar@gmail.com>
2023-10-17 08:51:36 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int x = 0;
+
+    while (x < 10)
+    {
+        printf("%d", x);
+        x++;
+    }
+    printf("\n");
+    return (0);
+}
+
