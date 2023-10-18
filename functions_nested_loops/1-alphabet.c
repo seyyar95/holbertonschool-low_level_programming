2e4d03a254ab0828715c48452f2316f25a17b628
seyyar95 <heydrovsyyr@gmail.com>
2023-10-18 03:29:03 -0700

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabet
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	
+}
