31d7f65f70949d215c08bdb900344e2a3bfbcc90
seyyar95 <heydrovsyyr@gmail.com>
2023-10-19 05:47:46 -0700

    lear your times

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i, n, sum;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			sum = n * i;
+
+			if (sum >= 10)
+			{
+				_putchar(sum / 10 + '0');
+				_putchar(sum % 10 + '0');
+			}
+
+			else
+			{
+				_putchar(sum + '0');
+			}
+
+			if (i == 9)
+			{
+				_putchar('\n');
+				continue;
+			}
+			if (n * (i + 1) >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+}
