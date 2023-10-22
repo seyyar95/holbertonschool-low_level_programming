2376e303db3bad3b49521a1acfa9fca31a909f6b
seyyar95 <heydrovsyyr@gmail.com>
2023-10-22 12:06:48 -0700

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always  0
+ *
+ */
+
+int main(void)
+{
+	int i = 3;
+
+	printf("1 2");
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else
+		{
+			if (i % 3 == 0)
+				printf(" Fizz");
+			else
+			{
+				if (i % 5 == 0)
+					printf(" Buzz");
+				else
+					printf(" %d", i);
+			}
+		}
+		i++;
+	}
+
+	printf("\n");
+	return (0);
+}
