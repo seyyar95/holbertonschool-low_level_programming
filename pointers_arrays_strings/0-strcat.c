cb6533a740754c98e8a187596f6216aa43cca050
seyyar95 <heydrovsyyr@gmail.com>
2023-10-26 05:24:52 -0700

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destlen = strlen(dest) - 1;
+	int srclen = strlen(src) - 1;
+	int i;
+
+	for (i = 0; i <= srclen; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
