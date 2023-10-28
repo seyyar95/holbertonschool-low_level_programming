8a4ec2c254683844be5a380b332d63644a8852f8
seyyar95 <heydrovsyyr@gmail.com>
2023-10-28 01:28:58 -0700

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string
+ * @s: string to be encoded
+ * Return: the result string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char b[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
