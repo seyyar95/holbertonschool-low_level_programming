eac4683345687345432c9ad2742a89d9ec30883d
seyyar95 <heydrovsyyar@gmail.com>
2023-10-28 09:56:05 -0700

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		char a = s[i];
+
+		if (a == c)
+		{
+			return (s + i);
+		}
+
+		if (a == '\0')
+		{
+			return (0);
+		}
+	}
+}
