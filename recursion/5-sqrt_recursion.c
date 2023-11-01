1895854d16812d72b64ab3b9e6c8b4c1b5ac7637
seyyar95 <heydrovsyyr@gmail.com>
2023-11-01 03:31:07 -0700

    Your subconscious is looking for the dreamer

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+*my_sqrt - recursion  that returns the
+*natural square root of a number
+*
+*@n: number
+*
+*@i: iteratior
+*
+*@c: counter
+*
+*Return: square root
+*/
+
+int my_sqrt(n, i, c)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (c);
+	}
+
+	return (my_sqrt(n - i, i + 2, c + 1));
+}
+
+/**
+ * _sqrt_recursion - a function that returns the natural
+ * square root of a number
+ *
+ * @n: number
+ *
+ *Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (my_sqrt(n, 1, 0));
+}
