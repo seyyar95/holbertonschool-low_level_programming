dae72f05f48bc35393a2b1cef05e8128a64db5a6
seyyar95 <heydrovsyyr@gmail.com>
2023-12-04 23:04:06 -0800

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,14 +2,14 @@
 #include "lists.h"
 
 /**
- * print_list - a function that prints all the elements of list_t list
- *
- * @h: a pointer to the list_t list
- *
- * Return: the number of nodes
- *
- *
- */
+*print_list - a function that prints all the elements of list_t list
+*
+*@h: a pointer to the list_t list
+*
+*Return: the number of nodes
+*
+*
+*/
 
 size_t print_list(const list_t *h)
 {
