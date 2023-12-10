44a4513fe759557db5d63c3ba3e42bf8f35a4eaf
seyyar95 <heydrovsyyr@gmail.com>
2023-12-10 11:24:51 -0800

    clear bit

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit 0
+ * @n: A pointer to the bit.
+ * @index: The index to set the value
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 1.
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(*n) * 8))
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
