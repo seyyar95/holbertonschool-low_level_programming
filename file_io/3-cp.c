ca237f96efe43b1df96e082520dbeac2ffd34fb9
seyyar95 <heydrovsyyar@gmail.com>
2023-12-12 04:56:09 -0800

    hope

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 		w = write(fd2, buffer, r);
 		if (w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	} while (r > 0);
