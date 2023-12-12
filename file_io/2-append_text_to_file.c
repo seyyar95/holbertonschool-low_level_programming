a619065b388396abd1454506170a73bb56aed485
seyyar95 <heydrovsyyar@gmail.com>
2023-12-12 01:11:46 -0800

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t w;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
