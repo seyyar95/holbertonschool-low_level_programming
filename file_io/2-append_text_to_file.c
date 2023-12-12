2b898583d1667829413ad5a3548cf88f18252da7
seyyar95 <heydrovsyyar@gmail.com>
2023-12-12 01:16:21 -0800

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,14 +12,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0;
 	ssize_t w;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
 	fd = open(filename, O_WRONLY | O_APPEND);
-	w = write(fd, text_content, strlen(text_content));
+	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
